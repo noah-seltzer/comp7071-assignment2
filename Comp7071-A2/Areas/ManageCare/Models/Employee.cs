706ee3490915ffc91ef1766579a8a4d204e577c5
Ravinder Shokar <ravindershokar@gmail.com>
2025-02-25 18:02:20 -0800

    Updated database

---
+++ Comp7071-A2/Areas/ManageCare/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Comp7071_A2.Areas.ManageCare.Models
+{
+    public class Employee
+    {
+
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Address { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string EmergencyContactPhone { get; set; }
+    
+        public required string JobTitle { get; set; }
+
+        public ICollection<Schedule> Schedule { get; set; } = [];
+
+        public ICollection<Certification> Certifications { get; set; } = [];
+    }
+}
