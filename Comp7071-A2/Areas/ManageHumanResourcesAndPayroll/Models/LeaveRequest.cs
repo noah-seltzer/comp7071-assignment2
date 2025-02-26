a682b8f32116238fd5aa71871cf281436eade548
Abbe Azale <abbeazale98@gmail.com>
2025-02-26 00:04:31 -0800

    added models for HR

---
+++ Comp7071-A2/Areas/ManageHumanResourcesAndPayroll/Models/LeaveRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Comp7071_A2.Areas.ManageHumanResourcesAndPayroll.Models
+{
+    public class LeaveRequest
+    {
+        public DateTime Start_Date { get; set; }
+
+        public DateTime End_Date { get; set; }
+        
+        public int Total_Days { get; set; }
+
+        public bool approval { get; set; }
+
+        public enum leave_type
+        {
+            sick,
+            vacation
+        }
+    }
+}
---
+++ Comp7071-A2/Areas/ManageHumanResourcesAndPayroll/Models/Shift.cs
@@ -0,0 +1,15 @@
+﻿namespace Comp7071_A2.Areas.ManageHumanResourcesAndPayroll.Models
+{
+    public class Shift
+    {
+        public float Hours_Worked { get; set; }
+       
+        public DateTime Start_Time { get; set; }
+
+        public enum Status
+        {
+            upcoming, sick, worked, vacation
+        }
+
+    }
+}
